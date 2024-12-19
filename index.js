c97891adb6f11a8d9c6417c005eaa7af4fa05ecb
POORNESH PRAHALATHAN <83974934+LoheshPrahal@users.noreply.github.com>
2024-12-19 21:30:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    console.log("WE GOT A NEW REQUEST!!");
-    res.send("Hello! We got your request!");
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!!");
+//     // res.send("Hello! We got your request!");
+//     // res.send({ color: 'red' });
+//     // res.send("<h1>This is my webpage</h1>");
+// })
+
+app.get('/', (req, res) => {
+    res.send('This is the homepage!');
 })
 
+app.post('/cats', (req, res) => {
+    res.send('Post request to /cats!!! This is different than a get request!!')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!!');
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('Woof!');
+})
+// /cats => 'meow'
+// /dogs => 'woof'
+// / => home
+
+
+app.get('*', (req, res) => {
+    res.send(`I don't know that path!`);
+})
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080!")
 })
